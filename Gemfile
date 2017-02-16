8423b86bbec1bf29bd428ab4d21f5a5543c32709
JadziaHax <madelyn.a.thomas@gmail.com>
2017-02-16 12:55:51 -0800

    Adds seeder file and faker and awesomeprint gems for it.

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem 'bcrypt', '~> 3.1.7'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  gem 'faker'
+  gem 'awesome_print'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,57 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Appointment.destroy_all
+
+def datetime_in_next_week
+  now           = DateTime.now
+  week_from_now = DateTime.now + 7
+
+  time_between  = Time.at((week_from_now.to_f - now.to_f)*rand + now.to_f).to_s
+
+  DateTime.parse( time_between )
+end
+
+coinflip_array = [ true, false ]
+
+users_array = 26.times.map do
+  flip_result = coinflip_array.sample
+  flip_result ? subject_matter = Faker::Pokemon.name : subject_matter = nil
+  flip_result ? current_phase = rand(2..3) : current_phase = rand(1..2)
+
+  User.create!( name: Faker::Name.first_name,
+                email: Faker::Internet.email,
+                password_digest: 'password', # this should be the encrypted password
+                phase: current_phase,
+                is_mentor: flip_result,
+                expertise: subject_matter)
+end
+
+mentors_array = users_array.find_all do |user|
+  user.is_mentor = true
+end
+
+students_array = users_array - mentors_array
+
+
+appointments_array = []
+
+# binding.pry
+mentors_array.each do |mentor|
+  number_of_slots = rand(1..4)
+  number_of_slots.times.map do
+    new_appointment = Appointment.create!( mentor_id: mentor.id,
+                                           start_time: datetime_in_next_week)
+    appointments_array << new_appointment
+  end
+end
+
+available_appointments = appointments_array.find_all do |appointment|
+  appointment.student_id =  nil
+end
+
+
+
+students_array.each do |student|
+  if available_appointments != []
+    available_appointments.sample.student_id = student.id
+  end
+end

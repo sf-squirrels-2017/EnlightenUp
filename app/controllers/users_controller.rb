acd0abc1694bdee4fe2fd1ac108e16563c45edee
JadziaHax <madelyn.a.thomas@gmail.com>
2017-02-16 18:39:03 -0800

    Merge to get sessions_helper conflict integrated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,14 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+
+    if is_current_user?(@user)
+      render 'show'
+    elsif !is_current_user?(@user) && @user.is_mentor?
+      render 'show'
+    elsif !is_current_user?(@user)
+      render '/not_authorized'
+    end
   end
 
   def edit
